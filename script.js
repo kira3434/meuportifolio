f3afccfc329d9a3548ceefa8a5d007864ecce89d
Jhonatas Reis <83553776+kira3434@users.noreply.github.com>
2023-05-31 23:09:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 document.addEventListener('DOMContentLoaded', function() {
     // Obtém o botão de alternância e o elemento body
     const toggleButton = document.getElementById('toggle-mode');
@@ -17,5 +18,41 @@ document.addEventListener('DOMContentLoaded', function() {
     AOS.init({
       duration:2000
     });
-  });
 
+
+    const slides = document.querySelectorAll('.slide');
+const thumbnails = document.querySelectorAll('.thumbnail');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+
+let currentSlide = 0;
+let slideInterval = setInterval(nextSlide, 15000);
+
+function nextSlide() {
+  goToSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+  goToSlide(currentSlide - 1);
+}
+
+function goToSlide(n) {
+  slides[currentSlide].classList.remove('active');
+  thumbnails[currentSlide].classList.remove('active');
+  currentSlide = (n + slides.length) % slides.length;
+  slides[currentSlide].classList.add('active');
+  thumbnails[currentSlide].classList.add('active');
+}
+
+prevButton.addEventListener('click', () => {
+  clearInterval(slideInterval);
+  prevSlide();
+});
+
+nextButton.addEventListener('click', () => {
+  clearInterval(slideInterval);
+  nextSlide();
+});
+
+
+  });
